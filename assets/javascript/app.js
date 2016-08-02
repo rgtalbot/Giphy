a1c48b901e7634e57f4cd9c79d7996dc26de8222
rgtalbot <rgtalbot@users.noreply.github.com>
2016-08-02 17:48:19 -0400

    changed to seinfeld theme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,51 @@
 $(document).ready(function () {
 
 
-    $(".search").on('click', function () {
-        var search = $(this).data('query');
+    var array = ["Jerry", "Costanza", "Elaine", "Kramer"];
+
+    function renderButtons() {
+        $('#buttonView').empty();
+        $.each(array, function (index, value) {
+            var addedButton = $('<button>')
+                .addClass('search btn btn-primary')
+                .attr('data-name', value)
+                .html(value);
+            $('#buttonView').append(addedButton);
+        });
+    }
+
+    function displayGIF() {
+        console.log('working');
+        var search = $(this).data('name');
         $.ajax({
-            url: "http://api.giphy.com/v1/gifs/search",
+            url: "https://api.giphy.com/v1/gifs/search",
             data: {
-                limit: 21,
+                limit: 15,
                 api_key: "dc6zaTOxFJmzC",
-                q: search
+                q: search + '+seinfeld'
             }, method: 'GET'
         })
             .done(function (response) {
                 $('#results').empty();
                 $.each(response.data, function (key, gif) {
-                    $('#results').append('<iframe src ="' + gif.embed_url + '",/>');
+                    var displayImage = $('<img>')
+                    .addClass('image')
+                        .attr('src', gif.images.original.url);
+                    $('#results').append(displayImage);
                 })
             });
-    });
+    }
 
+    $('#addGIF').on('click', function() {
+        var textInput = $('#gif-input').val();
+        if (textInput != "") {
+            array.push(textInput);
+            renderButtons();
+        }
+        $('#gif-input').val("");
+        return false;
+    })
+    $(document).on('click', '.search', displayGIF);
+
+    renderButtons();
 });
